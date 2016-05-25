a05f1a5e98e7716c3d36a86cb6a56438eb48d912
Atanas Vasilev <Atanas Vasilev>
2016-05-25 18:38:51 +0300

    Adding main file and requests module folder

---
+++ main.py
@@ -0,0 +1,43 @@
+import requests
+import warnings
+#import json
+import sys
+
+def testConnection(token):
+  print("\n Connecting to REST API...")
+  headers = {"Authorization":"Bearer " + token}
+  response = requests.get("https://api-fxpractice.oanda.com/v3/accounts", headers=headers)
+  if response.status_code is 200:
+    print("\n----------------------------------")
+    print("Connection successful !")
+    json = response.json()
+    accountID = json["accounts"][0]["id"];
+    print("\nYour accountID is : " + accountID)
+    return accountID
+  else:
+    print(response.status_code)
+
+
+print("\n Hello welcome to my trading platform !")
+print("\nEnter your personal key to continue : ")
+token = input("$> ")
+
+with warnings.catch_warnings():
+  warnings.simplefilter("ignore")
+  accountID = testConnection(token)
+
+print("\n")
+while 1:
+  print("How would you like to continue ?")
+  print("(monitor | trade | automation | abort)")
+  choice = input("$> ")
+  if choice is "abort":
+    print("\nRight choice for now !")
+    break
+  else:
+    print("\nInvalid choice!")
+
+
+
+
+
