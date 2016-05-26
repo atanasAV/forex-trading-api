d465fe20f2d138748ebf2929a1ec902d37695682
atanas <atanas.vasilev.av@gmail.com>
2016-05-26 16:44:27 +0300

    Adding folder trading-modules

---
+++ trading-module/monitor.py
@@ -0,0 +1,19 @@
+import requests
+import warnings
+import sys
+
+class Monitor:
+
+	def __init__(self, accountID, token):
+		self.token = token
+		self.pricingApi = "https://api-fxpractice.oanda.com/v3/accounts/"
+		self.pricingApi += accountID
+		self.pricingApi += "/pricing"
+
+	#currencies is a list of strings
+	def getBidsAndAsks(self, currencies):
+		headers = {"Authorization":"Bearer " + self.token}
+		params = {"instruments":currencies}
+		response = requests.get(self.pricingApi, headers=headers, params=params)
+		if response.status_code == 200:
+			return response.json()["prices"][0]
